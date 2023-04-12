37039e71cfcaa5468e0d6a09ed5b8f79c0f3cac8
Satyam <satyam19arya@gmail.com>
2023-04-12 20:33:35 +0530

    title changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <nav>
         <div className="banner">
-          My Shopify
+          My Collections
         </div>
 
         <div className="right-layout">
